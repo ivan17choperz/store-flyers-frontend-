3588bf1038e0b9cbc6282331d042edf20e16284c
ivan17choperz <ivan17choperz@gmail.com>
2024-09-01 21:59:01 -0500

    changes page about-us

--- src/shared/pages/about-us/about-us.page.vue
+++ src/shared/pages/about-us/about-us.page.vue
@@ -1,5 +1,64 @@
 <template>
-  <div>
-    <h2>about us</h2>
+  <div class="w-full px-5 text-sky-600">
+    <div class="flex flex-col justify-between">
+      <div class="pt-10 px-20 flex items-center">
+        <div class="w-6/12">
+          <h3 class="text-3xl font-bold mb-4">Esta es nuestra historia</h3>
+          <article class="text-lg">
+            <p>
+              Somos un ministerio cristiano que nació como producto del esfuerzo
+              de un grupo de creyentes en Cristo en Colombia llamados a servir
+              en esta actividad del evangelismo y que vimos la necesidad de
+              recursos eavngelísticos impresos basados en la Palabra de Dios
+              para promulgar las buenas nuevas de salvación.
+            </p>
+            <br />
+            <p>
+              Desde el año 2004 con el apoyo inicial de la Fundación Semillas se
+              han producido varios millones de estos tratadoseangelísticos que
+              han sido distribuidos en el territorio colombiano y varios países
+              de latinoamérica y Estados Unidos.
+            </p>
+            <br />
+            <p>
+              A partir del año 2011 iniciamos a dar a conocer nuestro ministerio
+              a través de internet con el fin de impulsar esta obra y con la
+              ayuda del Señor llevar el mensaje del evangelio de forma clara,
+              eficaz y con lenguaje actual a muchas más personas en el mundo
+              para que sean impactadas y llevadas al pies del Señor Jesucristo.
+            </p>
+          </article>
+        </div>
+        <div class="w-6/12 flex justify-end">
+          <img
+            src="../../../assets/media/imagen-critiana-1.jpg"
+            class="border-[15px] border-white shadow-2xl w-[500px] h-auto"
+            alt="img-critiana"
+          />
+        </div>
+      </div>
+      <div class="text-center mt-10">
+        <article class="px-20 py-10 border-[10px] border-sky-600">
+          <h2 class="text-3xl font-bold">Mision</h2>
+          <br />
+          <p class="text-xl">
+            Contribuir con la difusión del evangelio del Señor Jesucristo, a
+            través de mensajes claros y eficaces plasmados en folletos o
+            tratados evangelísticos, impulsando, motivando y estimulando su
+            distribución de forma continua por parte del pueblo cristiano.
+          </p>
+        </article>
+        <article class="px-20 py-10 border-[10px] border-sky-600 my-5">
+          <h2 class="text-3xl font-bold">Visión</h2>
+          <br />
+          <p class="text-xl">
+            Alcanzar millones de almas para Dios por medio de la literatura
+            evangelística cuyo contenido atractivo, actual y contundente sea de
+            interés para lectores de todas las clases, culturas y edades
+            conduciéndoles a los pies del Señor Jesucristo.
+          </p>
+        </article>
+      </div>
+    </div>
   </div>
 </template>
